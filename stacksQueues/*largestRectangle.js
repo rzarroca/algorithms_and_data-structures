33f49c42b25e1ff1ac3bb78ad8ece730de1ca094
Ricardo Zarroca <ricardozarroca@gmail.com>
2021-12-03 09:21:48 +0000

    Stacks: not found a stack solution
    Doing pair programming with Martin and he found a difficult solution

---
+++ stacksQueues/*largestRectangle.js
@@ -0,0 +1,18 @@
+const sample1 = [1, 2, 3, 4, 5]
+const sample2 = [1, 3, 5, 9, 11]
+const sample3 = [11, 11, 10, 10, 10]
+
+// super hard implementation... even for catman
+
+function largestRectangle(h) {
+  let max = 0
+  for (let i = 0; i < h.length; i++) {
+    const height = h[i];
+    while (i !==0 && h[i]) {
+      
+    }
+    
+    
+  }
+
+}
